3468682e85381538f4918a8c5ac62662af1d765b
Muhammad Sofyan <octa7th@gmail.com>
2013-11-04 10:33:02 +0700

    Add method toProperCase for string object

--- helper.js
+++ helper.js
@@ -100,6 +100,12 @@ String.method('toInt', function (radix) {
 	return parseInt(this, radix);
 });
 
+String.method('toProperCase', function () {
+	return this.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+});
+
 
 var by = function (name) {
 	var minor, order, caseSensitive;
